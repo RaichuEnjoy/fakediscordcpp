a464624ac5321717be727902feaa574dc15d3514
legocoder123 <legocoder123@gmail.com>
2022-12-04 19:37:20 -0600

    epic code and exe

---
+++ fake discord cpp/chat.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string msg;
+    cout << "For Real It's Discord No Question About It!" << endl;
+    while (true)
+    {
+        cin >> msg;
+        cout << "John[mod][bot][owner]: Fuck you dumb shit." << endl;
+        cout << "BobNebo333[user][kid*******][peaseant]: Send Feet If You Are Kid!" << endl;
+        cout << "AnimeEGirl[Girl][Animefan][Kitten]: UwU" << endl;
+    }
+
+    return 0;
+}
